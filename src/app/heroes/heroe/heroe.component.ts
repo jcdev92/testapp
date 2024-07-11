d16f5eb660392e2771e89ea0568854c6de083952
jcdev92 <jcdeveloper92@gmail.com>
2024-07-11 11:50:40 -0400

    2 nuevos componente anadidos

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrl: './heroe.component.css'
+})
+export class HeroeComponent {
+
+}
