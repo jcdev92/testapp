aeae7a6767f6f20191a6b9ba3adf8bb4eb3ddd44
jcdev92 <jcdeveloper92@gmail.com>
2024-07-11 11:27:47 -0400

    finishing component counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'Mi Primera App con Angular';
+  public paragraph: string = 'Probando Angular, lorem ipsum dolor sit amet consectetur adipisicing elit. illum sapiente aliquid qui saepe voluptate voluptates ipsum quas! Hic dolorem aut aliquid facilis asperiores optio magnam provident expedita amet.';
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -6,15 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
-  public counter: number = 0;
+  public minCounterValue: number = 10;
+  public counter: number = this.minCounterValue;
 
   public increment(value: number): void {
     this.counter += value;
   }
 
   public decrement(value: number): void {
-    if (this.counter > 0) {
+    if (this.counter > this.minCounterValue) {
       this.counter -= value;
     }
+  }
+
+  public resetCounter(): void {
+    this.counter = this.minCounterValue;
   }
 }
