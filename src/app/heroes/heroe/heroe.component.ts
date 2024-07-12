4fa4aaeae8f0b05643eec8bc7e4f32c47b030694
jcdev92 <jcdeveloper92@gmail.com>
2024-07-11 21:55:29 -0400

    lista de nombres de heroes funcionando

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -8,6 +8,26 @@ import { Component } from '@angular/core';
 export class HeroeComponent {
   public name: string = 'Ironman';
   public age: number = 45;
-  
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroeDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeName(): void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 25;
+  }
+
+  resetForm(): void {
+    this.name = 'Ironman';
+    this.age = 45;
+  }
 
 }
--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  public title: string = 'List of Heroes';
+  public defaultNames: string[] = ['Batman', 'Superman', 'Ironman', 'Hulk', 'Thor'];
+  public heroesNameList: string[] = [...this.defaultNames];
+  public deletedHeroe?: string;
 
+  removeLastHeroe():void {
+    this.deletedHeroe = this.heroesNameList.pop();
+  }
+
+  resetList():void {
+    this.heroesNameList = [...this.defaultNames];
+  }
 }
